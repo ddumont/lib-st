68ee095e53079ced8c14bbc76a6383857d6d4553
ddumont <ddumont@e42ddd76-76d2-4f5a-b259-0aaa859aa59a>
2008-10-16 01:10:11 +0000

    lib-st: wrath change to scroll frames.
    git-svn-id: https://repos.wowace.com/wow/lib-st/trunk@10 e42ddd76-76d2-4f5a-b259-0aaa859aa59a

--- Core.lua
+++ Core.lua
@@ -178,8 +178,8 @@ do
 			end
 		end
 		
-		scrollframe:SetScript("OnVerticalScroll", function()
-			FauxScrollFrame_OnVerticalScroll(st.rowHeight, st.Refresh);
+		scrollframe:SetScript("OnVerticalScroll", function(self, offset)
+			FauxScrollFrame_OnVerticalScroll(self, offset, st.rowHeight, st.Refresh);
 		end);
 		
 		st.data = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22};
