12184917faee257b83482c54a42099b3cce06d0b
Paweł Jastrzębski <pawelj@iosphe.re>
2020-08-21 12:56:56 +0200

    Shadowlands update

--- Core.lua
+++ Core.lua
@@ -621,7 +621,7 @@ do
 	function lib:CreateST(cols, numRows, rowHeight, highlight, parent)
 		local st = {};
 		self.framecount = self.framecount or 1;
-		local f = CreateFrame("Frame", "ScrollTable" .. self.framecount, parent or UIParent);
+		local f = CreateFrame("Frame", "ScrollTable" .. self.framecount, parent or UIParent, BackdropTemplateMixin and "BackdropTemplate");
 		self.framecount = self.framecount + 1;
 		st.showing = true;
 		st.frame = f;
