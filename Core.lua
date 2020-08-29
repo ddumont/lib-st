696e91d8af5ad51f9d0d42edceef9a58010c0077
Dan Dumont <ddumont@gmail.com>
2020-08-29 17:30:24 -0400

    add license

--- Core.lua
+++ Core.lua
@@ -1,3 +1,18 @@
+-- Copyright 2008-2020 Dan Dumont
+--
+-- This program is free software: you can redistribute it and/or modify
+-- it under the terms of the GNU General Public License as published by
+-- the Free Software Foundation, either version 3 of the License, or
+-- (at your option) any later version.
+
+-- This program is distributed in the hope that it will be useful,
+-- but WITHOUT ANY WARRANTY; without even the implied warranty of
+-- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+-- GNU General Public License for more details.
+
+-- You should have received a copy of the GNU General Public License
+-- along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 local MAJOR, MINOR = "ScrollingTable", tonumber("@project-timestamp@") or 40300;
 local lib, oldminor = LibStub:NewLibrary(MAJOR, MINOR);
 if not lib then
